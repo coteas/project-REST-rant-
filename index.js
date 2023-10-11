62c7babef4d7d27fd528d5f0702bdb1052e97849
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-10 19:43:11 -0700

    Fixing some errors in file setup

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-// Require needed modules.
+require('dotenv').config()
 const express = require('express')
-
-// Initialize the app object.
 const app = express()
 
-// Create a homepage route.
-app.get('/', function (req, res) {
-    // This gets sent to the client 
-    // (your web browser most likely!)
-    res.send('Hello world')
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
-// Create a second route.
-app.get('/second', function (req, res) {
-    res.send('My Second Page!')
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
-// Listen for connections.
-app.listen(3000, function () {
-    console.log('I am awake!')
-})
+app.listen(process.env.PORT)
