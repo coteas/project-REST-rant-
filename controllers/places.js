adf44a83d04612b6efcf0687608dd21ccf75f7c1
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-16 16:05:44 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,28 @@
 const router = require('express').Router()
+const places = require('../models/places')
 
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
 
   router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-  })
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+res.redirect('/places')  
+})
   
+
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/katlyn-giberson-OkhcMbf3vQ0-unsplash.jpg'
-      }, {
-          name: 'Coding Cat Cafe',
-          city: 'Phoenix',
-          state: 'AZ',
-          cuisines: 'Coffee, Bakery',
-          pic: '/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg'
-      }]
-      
+    
     res.render('places/index', {places})
 })
 
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/katlyn-giberson-OkhcMbf3vQ0-unsplash.jpg'
+  }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg'
+  }]
