0cfb1987f9ff85bf6275a81c936c2b25bfbf8929
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-16 15:41:53 -0700

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
