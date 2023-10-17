f3fe8569a2f327d8de2d5cccbab576fdcf2f7af5
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-16 19:20:59 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -5,11 +5,23 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-               
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                        <a href="/places">Places</a>
+                        </li>
+                        <li>
+                        <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
--- views/home.jsx
+++ views/home.jsx
@@ -6,16 +6,16 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
-                <a href="/places">
-                    <button className="btn-primary">Places Page</button>
-                </a>
+               
                 <div>
                   <img src="/images/brenda-godinez-MsTOg6rhRVk-unsplash.jpg" alt="Chia Fruit Shake"/>
                 <div>
                   Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk"> Unsplash </a>
                 </div>
                 </div>
-                
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                    </a>
             </main>
         </Def>
     )
