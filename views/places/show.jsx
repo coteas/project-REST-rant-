50410028fd5006dbf9df776e1f398997aaad93c5
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-16 19:41:08 -0700

    troubleshooting

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,17 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+            <p>Not rated</p>
+            <h2>Description</h2>
+            <p>No description yet</p>
+            <h2>Comments</h2>
+            <p>No comments yet!</p>
           </main>
         </Def>
     )
