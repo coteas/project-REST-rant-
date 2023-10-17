7d9859fa6fbf8ed28ace26800037df995f10f9d4
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-16 19:24:09 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,26 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
+
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id] })
+    }
+  })
+  
+  
+  
 
   router.post('/', (req, res) => {
     if (!req.body.pic) {
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,20 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
-        return (
-          <div className="col-sm-6">
-            <h2>{place.name}</h2>
-            <p className="text-center">
-              {place.cuisines}
-            </p>
-            <img src={place.pic} alt={place.name} />
-            <p className="text-center">
-              Located in {place.city}, {place.state}
-            </p>
-          </div>
-        )
-      })
+  let placesFormatted = data.places.map((place, index) => {
+    return (
+      <div className="col-sm-6">
+        <h2>
+          <a href={`/places/${index}`} >
+            {place.name}
+          </a>
+        </h2>
+        <p className="text-center">
+          {place.cuisines}
+        </p>
+        <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    )
+  })
+  
       
     return (
       <Def>
