80044720fa4c5e9f5bfcb55778e61f72c42fdee6
Madison Ciccarelli <mciccadesign@gmail.com>
2023-10-18 17:26:13 -0700

    Added the edit  functionality

--- controllers/places.js
+++ controllers/places.js
@@ -47,6 +47,36 @@ router.get('/new', (req, res) => {
     }
   })   
   
+  //put route
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+  
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+  
+  
+  
     router.post('/', (req, res) => {
     if (!req.body.pic) {
       // Default image if one is not provided
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,6 +5,10 @@ function show (data) {
     return (
         <Def>
           <main>
+            <div className='images-for-landing'>
+          <img src= {data.place.pic} />
+          </div>
+          <div className='data-ins'>
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
             <p>Not rated</p>
@@ -13,17 +17,17 @@ function show (data) {
             <h2>Comments</h2>
             <p>No comments yet!</p>
             {/* Edit and Delete buttons */}
+            <div className='buttons'>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                     Edit
-                </a>  
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form> 
-   
-
-
+                </a>     
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+                </div>
+                </div>
           </main>
         </Def>
     )
